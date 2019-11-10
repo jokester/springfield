d4492bdc5e667ae2057497dcfdb7651a99dd0fe3
Wang Guan <momocraft@gmail.com>
2019-11-10 10:22:47 +0900

    add cra-build demo to docs/

---
+++ docs/demo/precache-manifest.07b2534e93f6f431bc8754acfa16a699.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec441c99e11f4a986cef2f2febd65e27",
+    "url": "/springfield/demo/index.html"
+  },
+  {
+    "revision": "45c5b54e648333f0c45b",
+    "url": "/springfield/demo/static/js/2.4d0847b5.chunk.js"
+  },
+  {
+    "revision": "e17e12a6771f97a365f3",
+    "url": "/springfield/demo/static/js/main.20d2695b.chunk.js"
+  },
+  {
+    "revision": "fee3b967fbe9fddf544c",
+    "url": "/springfield/demo/static/js/runtime-main.8a938b16.js"
+  }
+]);
